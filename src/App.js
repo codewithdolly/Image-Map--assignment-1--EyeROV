4829959fe2db1b85c1ecbe223ce76169aec60be6
hicoderindia <hicoder.dolly@gmail.com>
2022-01-06 02:55:39 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import './App.scss';
-import { Form, ImgMap } from './Components';
+import React from "react";
+import "./App.scss";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Form, ImgMap } from "./Components";
 
 function App() {
+  const [value, setValue] = React.useState("one");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <>
-    <div className="App">
-
-     <ImgMap />
-     <Form />
-    </div>
+      <BrowserRouter>
+        <div className="app">
+          <Tabs value={value} onChange={handleChange}>
+            <Link to="/map">
+              <Tab value="one" label="Tast 1" className="tab" />
+            </Link>
+            <Link to="/form">
+              <Tab value="two" label="Tast 2" className="tab" />
+            </Link>
+          </Tabs>
+          <Routes>
+            <Route path="/map" element={<ImgMap />} />
+            <Route path="/form" element={<Form />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
