9338a18b50802d163d3c6e5335ca80bf9563b324
hicoderindia <hicoder.dolly@gmail.com>
2022-01-06 01:09:24 +0530

    Task one completed

--- src/Components/ImgMap/ImgMap.js
+++ src/Components/ImgMap/ImgMap.js
@@ -1,36 +1,47 @@
-import React from 'react'
-import "./ImgMap.scss"
+import React, { useState } from "react";
+import "./ImgMap.scss";
 import sonarmap from "../Images/map.png";
 
-const imgObj= [
-    {
-        "title": "Step - Apron step",
-        "coords": "55,167,764,270",
-        "shape":"rect"
-    },
-    {
-        "title": "Crater - at location 1",
-        "coords": "591,19,690,145",
-        "shape":"rect"
-    }
-]
-
+const imgObj = [
+  {
+    title: "Step - Apron step",
+    coords: "55,167,764,270",
+    shape: "rect",
+  },
+  {
+    title: "Crater - at location 1",
+    coords: "591,19,690,145",
+    shape: "rect",
+  },
+];
 
 const ImgMap = () => {
-    return (
-        <>
-        <div className='imgMap'>
-           <img src={sonarmap} alt="sonarmap" useMap="#workmap" /> 
-           <map name="workmap">
-           {imgObj.map((obj)=>{
-               return(
-                <area shape={obj.shape} coords={obj.coords} href="#" alt={obj.title} />
-               )
-           })}
-           </map>
-           </div>
-        </>
-    )
-}
+  const [isShown, setIsShown] = useState(false);
+
+  return (
+    <>
+      <div className="imgMap">
+        <img src={sonarmap} alt="sonarmap" useMap="#workmap" />
+        <map name="workmap">
+          {imgObj.map((obj) => {
+            return (
+              <>
+                <area
+                  shape={obj.shape}
+                  coords={obj.coords}
+                  href="#"
+                  alt={obj.title}
+                  onMouseEnter={() => setIsShown(true)}
+                  onMouseLeave={() => setIsShown(false)}
+                />
+                {isShown && <div>{obj.title}</div>}
+              </>
+            );
+          })}
+        </map>
+      </div>
+    </>
+  );
+};
 
 export default ImgMap;
